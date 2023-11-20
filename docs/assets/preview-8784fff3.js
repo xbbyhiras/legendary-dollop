0ef5da720571587633a69b0cc586c3c835a86f16
xavier-brinon <xavier.brinon@byhiras.com>
2023-11-20 11:44:33 +0000

    docs: :memo: build storybook page in docs folder

--- docs/assets/preview-8784fff3.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-ef242289.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-7e6cb61f.js"),["./DocsRenderer-NNNQARDV-7e6cb61f.js","./iframe-ef242289.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-e24abcef.js","./index-a764d3ff.js","./index-726b51c3.js","./index-d37d4223.js","./isNativeReflectConstruct-8ef081c8.js","./index-13a69483.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-8784fff3.js.map
