6f80a8cb8adc9cdd4e252fccd70176aed8f68f1b
xavier-brinon <xavier.brinon@byhiras.com>
2023-11-20 11:42:08 +0000

    docs: :memo: build storybook page

--- storybook-static/assets/preview-7d1a2b0b.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-005ce62d.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-7f25301c.js"),["./DocsRenderer-NNNQARDV-7f25301c.js","./iframe-005ce62d.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-6baef5c8.js","./index-da03a860.js","./index-c60d1cab.js","./index-d37d4223.js","./isNativeReflectConstruct-26aa4442.js","./index-13a69483.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-7d1a2b0b.js.map
