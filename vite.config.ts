71687faf05631d5f297ff4014919524b76f5d960
xavier-brinon <xavier.brinon@byhiras.com>
2023-11-23 06:43:11 +0000

    Add new UI components and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ['lib']})],
   assetsInclude: ['**/*.md'],
   build: {
+    copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, 'lib/index.ts'),
-      name: 'common-ui',
       formats: ['es'],
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'styled-components'],
+      external: ['react', 'react-dom', 'styled-components', 'react/jsx-runtime', 'antd'],
     },
   },
 })
