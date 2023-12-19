a93e9ca49a46a6c6cfcf5650eaf9ede244b1a7f0
xbbyhiras <xavier.brinon@byhiras.com>
2023-12-19 13:49:49 +0000

    chore: :wastebasket: remove storybook assets
    as this only need to be on local since it's being build during gh-a

--- docs/components/Alerts/AlertSpan.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-export declare const AlertSpan: import("styled-components").IStyledComponent<"web", import("styled-components/dist/types").Substitute<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>, {
-    $type?: "warning" | "error" | undefined;
-}>>;
--- docs/components/Article/Article.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-type TArticleProps = {
-    children: React.ReactNode;
-    className?: string;
-    enableScrolls?: boolean;
-};
-export declare const Article: ({ children, className, enableScrolls }: TArticleProps) => import("react/jsx-runtime").JSX.Element;
-export {};
